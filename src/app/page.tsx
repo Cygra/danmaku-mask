bbce2abcea81032ff1d969aad713bed6d6f137b9
Cygra <sjtuwbh@gmail.com>
2025-01-10 23:04:50 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,117 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef } from "react";
+
+import {
+  ImageSegmenter,
+  FilesetResolver,
+  ImageSegmenterResult,
+} from "@mediapipe/tasks-vision";
+
+const legendColors = [
+  [147, 170, 0, 255],
+  [166, 189, 215, 255],
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const process = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream;
+    }
+
+    let lastWebcamTime = -1;
+    const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+
+    const imageSegmenter = await ImageSegmenter.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath:
+          "https://storage.googleapis.com/mediapipe-models/image_segmenter/deeplab_v3/float32/1/deeplab_v3.tflite",
+        delegate: "GPU",
+      },
+      outputCategoryMask: true,
+      outputConfidenceMasks: false,
+      runningMode: "VIDEO",
+    });
+
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    const video = videoRef.current;
+
+    const callbackForVideo = (result: ImageSegmenterResult) => {
+      const imageData = ctx?.getImageData(
+        0,
+        0,
+        video!.videoWidth || 640,
+        video!.videoHeight || 480
+      ).data;
+      const mask = result?.categoryMask?.getAsFloat32Array();
+
+      let j = 0;
+      if (mask && imageData) {
+        for (let i = 0; i < mask.length; ++i) {
+          const maskVal = Math.round(mask[i] * 255.0);
+          const legendColor = legendColors[maskVal % legendColors.length];
+          imageData[j] = legendColor[0] + imageData[j];
+          imageData[j + 1] = (legendColor[1] + imageData[j + 1]) / 2;
+          imageData[j + 2] = (legendColor[2] + imageData[j + 2]) / 2;
+          imageData[j + 3] = (legendColor[3] + imageData[j + 3]) / 2;
+          j += 4;
+        }
+        const uint8Array = new Uint8ClampedArray(imageData.buffer);
+        const dataNew = new ImageData(
+          uint8Array,
+          video!.videoWidth || 640,
+          video!.videoHeight || 480
+        );
+        ctx?.putImageData(dataNew, 0, 0);
+        requestAnimationFrame(predictWebcam);
+      }
+    };
+
+    const predictWebcam = async () => {
+      if (!video || !canvasRef.current) return;
+      if (video.currentTime === lastWebcamTime) {
+        requestAnimationFrame(predictWebcam);
+        return;
+      }
+
+      lastWebcamTime = video.currentTime;
+      ctx?.drawImage(
+        video,
+        0,
+        0,
+        video!.videoWidth || 640,
+        video!.videoHeight || 480
+      );
+      if (imageSegmenter === undefined) {
+        return;
+      }
+      const startTimeMs = performance.now();
+      imageSegmenter.segmentForVideo(video, startTimeMs, callbackForVideo);
+    };
+
+    predictWebcam();
+  };
+
+  useEffect(() => {
+    process();
+  }, []);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  return (
+    <div className="flex items-center min-h-screen p-8 w-full justify-center">
+      <canvas ref={canvasRef} width={640} height={480} className="w-[800px]" />
+
+      <video playsInline ref={videoRef} autoPlay className={"hidden"}></video>
     </div>
   );
 }
